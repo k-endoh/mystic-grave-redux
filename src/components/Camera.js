628766c2ee4977bebf192b58b5daa265f70de197
KatsuyaEndoh <katsuya@amalab-no-MacBook-Pro.local>
2017-03-31 14:05:50 +0900

    [add] components/Camera.js developing animation functions

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -12,14 +12,19 @@ export const socket = io(serverData.server.address);
 class Camera extends React.Component {
 
   componentWillMount() {
+
+    let self = this;
     // TODO: これでいいのか？
     socket.on('controller', function (data) {
-      if(data.operation == 'jump'){
+      if(data.operation === 'jump'){
         console.log('jump');
+        console.log(self.state, document.getElementById('anim'));
+
         let event = new Event('jump');
         document.getElementById('myCamera').dispatchEvent(event);
+        self.setState({ isFall: !self.state.isFall});
 
-      }else if(data.operation == 'forward'){
+      }else if(data.operation === 'forward'){
         console.log('forward');
       }
     });
@@ -27,6 +32,9 @@ class Camera extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      isFall : false
+    };
   }
 
   render() {
@@ -36,32 +44,17 @@ class Camera extends React.Component {
                 rotation="0 225 0"
                 raycaster
                 crawling-cursor="target: #cursor">
-        <a-cursor></a-cursor>
-
-        <a-animation begin="jump" end="fall" attribute="position" to="-7.5 7 -7.5" ease="ease-out-cubic"></a-animation>
-        <a-animation begin="fall" attribute="position" to="-7.5 -1 -7.5"></a-animation>
+        <a-cursor/>
+        <a-animation
+          id="anim"
+          begin="jump"
+          attribute="position"
+          to="-7.5 7 -7.5"
+          ease={this.state.isFall ? "ease-out" : "ease-in"}
+          />
       </a-camera>
     )
   }
 }
 
-Camera.propTypes = {
-  data: React.PropTypes.array.isRequired
-};
-
 export default Camera;
-
-// import React, { Component } from 'react';
-// import 'aframe';
-//
-// const Camera = (props) => (
-//     <a-camera id="myCamera"
-//               position="-7.5 -1 -7.5"
-//               rotation="0 225 0"
-//               raycaster
-//               crawling-cursor="target: #cursor">
-//         <a-cursor></a-cursor>
-//     </a-camera>
-// );
-//
-// export default Camera;
